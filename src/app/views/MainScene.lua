f9dddd65febfb21aed1d5cbccafe8067a85d120c
Your Name <you@example.com>
2015-11-10 20:31:03 +0800

    edit class

--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -9,32 +9,32 @@ function MainScene:onCreate()
 
     -- add HelloWorld label
     cc.Label:createWithSystemFont("Hello World", "Arial", 40)
-        :move(display.cx, display.cy + 200)
+        :move(display.cx-500, display.cy -500)
         :addTo(self)
     local map = ccexp.TMXTiledMap:create("tmx/class.tmx")
-    map:move(333,333)
+    map:move(333 - 500,333 - 500)
     	:addTo(self)
 
-    local layer = map:getLayer("route")
-    local size = layer:getLayerSize()
+    -- local layer = map:getLayer("route")
+    -- local size = layer:getLayerSize()
    
-    local gid = layer:getTileGIDAt(cc.p(19, 99-80))
-    print("gid", gid)
-        local gid = layer:getTileGIDAt(cc.p(18,99- 80))
-    print("gid", gid)
-        local gid = layer:getTileGIDAt(cc.p(20, 99-80))
-    print("gid", gid)
+    -- local gid = layer:getTileGIDAt(cc.p(19, 99-80))
+    -- print("gid", gid)
+    --     local gid = layer:getTileGIDAt(cc.p(18,99- 80))
+    -- print("gid", gid)
+    --     local gid = layer:getTileGIDAt(cc.p(20, 99-80))
+    -- print("gid", gid)
 
-    for m = 0,size.width -1 do
-        for n = 0, size.height -1 do
-            if  layer:getTileGIDAt(cc.p(m, n)) ~= 0 then
-                print("m", m)
-                print("n", n)
-                print("gid", layer:getTileGIDAt(cc.p(m, n)))
-                layer:setTileGID(layer:getTileGIDAt(cc.p(m, n)) + 1, cc.p(m, n))
-            end 
-        end 
-    end
+    -- for m = 0,size.width -1 do
+    --     for n = 0, size.height -1 do
+    --         if  layer:getTileGIDAt(cc.p(m, n)) ~= 0 then
+    --             print("m", m)
+    --             print("n", n)
+    --             print("gid", layer:getTileGIDAt(cc.p(m, n)))
+    --             layer:setTileGID(layer:getTileGIDAt(cc.p(m, n)) + 1, cc.p(m, n))
+    --         end 
+    --     end 
+    -- end
 end
 
 return MainScene
